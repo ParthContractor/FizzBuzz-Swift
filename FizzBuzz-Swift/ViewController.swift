9e30c63f916a3d968b7242fa6f7cbc9f28165c0e
Parth Contractor <contractor110@gmail.com>
2020-01-25 14:55:33 +1100

    Initial Commit

---
+++ FizzBuzz-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FizzBuzz-Swift
+//
+//  Created by Parth on 25/01/20.
+//  Copyright © 2020 Parth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
